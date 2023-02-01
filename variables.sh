516e959646b86d8e32f953a8dbbb6a0422c5cc13
Bhargav_Solanki <solankibhargav2013@gmail.com>
2023-02-01 10:31:05 -0500

    variable path

---
+++ variables.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# String variable
+a="Hello, "
+b="How are you?"
+c="$a$b"
+echo $c
+
+# Home variable
+home=$HOME
+echo "Home variable: $home"
+
+# Path variable
+path=$PATH
+echo "Path variable: $path"
+
+# User variable
+user=$USER
+echo "User variable: $user"
